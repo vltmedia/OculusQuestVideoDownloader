6689adea53731aa29b7a05c99ed76018716e6405
vltmedia <info@vltmedia.com>
2019-09-18 17:51:18 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Oculus_Quest_Video_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(47, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pull Video Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Export Folder Location";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(360, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(192, 316);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(357, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Detected Files on Oculus";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Files to Export";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Single File to Pull *";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(46, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(137, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Pull ALL Files";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "* Only if \"Put All Files\" is Unchecked";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(48, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 50);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Oculus Quest\r\nVideo Downloader";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(299, 339);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Oculus Quest Video Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Oculus_Quest_Video_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public string Location = @"Quest\Internal shared storage\Oculus\VideoShots";
+        public string[] VideoFiles;
+        public string commandd = "adb pull /sdcard/oculus/VideoShots \"LOCATION\"" + Environment.NewLine + "Exit" + Environment.NewLine  ;
+        public string commanddsingle = "adb pull /sdcard/oculus/VideoShots/REPFILE \"LOCATION\"" + Environment.NewLine + "Exit" + Environment.NewLine  ;
+
+        public void SendCommand()
+        {
+            string comm = commandd.Replace("LOCATION", Location);
+            Console.WriteLine(checkBox1.CheckState);
+
+            if (checkBox1.CheckState == CheckState.Unchecked)
+            {
+                string filn = textBox2.Text;
+                if (!filn.Contains(".mp4"))
+                {
+                    filn = textBox2.Text + ".mp4";
+                }
+
+                comm = commanddsingle.Replace("LOCATION", Location).Replace("REPFILE", filn);
+                Console.WriteLine(comm);
+            }
+            else
+            {
+                comm = commandd.Replace("LOCATION", Location);
+                Console.WriteLine(checkBox1.CheckState);
+
+
+            }
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + comm;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+            MessageBox.Show("Completed Video Pull");
+
+        }
+
+        public void ProcessFiles()
+        {
+            try
+            {
+                string comm = commandd.Replace("LOCATION", Location);
+                Console.WriteLine(checkBox1.CheckState);
+
+                if (checkBox1.CheckState == CheckState.Unchecked) {
+                    string filn = textBox2.Text;
+                    if (!filn.Contains(".mp4")) { 
+                    filn = textBox2.Text + ".mp4";
+                    }
+
+                    comm = commanddsingle.Replace("LOCATION", Location).Replace("REPFILE", filn);
+                    Console.WriteLine(comm);
+                }
+                else
+                {
+                     comm = commandd.Replace("LOCATION", Location);
+                    Console.WriteLine(checkBox1.CheckState);
+
+
+                }
+                var process = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            UseShellExecute = true,
+                            RedirectStandardInput = true,
+                            //CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Normal
+                        }
+                    };
+                    Console.WriteLine(comm);
+                    process.Start();
+                    // For output data directly from command line.
+           
+                    // For error output directly from command line.
+                    //process.ErrorDataReceived += errorReceivedHandler;
+                    //process.BeginErrorReadLine();
+
+                    // For providing an command to command line.
+                    var command = comm;
+                    var writer = process.StandardInput;
+                    writer.WriteLine(command);
+                    //Thread.Sleep(3000);
+                    process.WaitForExit();
+                    process.Close();
+          
+
+            }
+            catch
+            {
+
+            }
+
+
+        }
+        public void FindQuest()
+        {
+
+            //Dealing with them in terms of them being removable disks is relatively easy, firstly finding the removable disks using DriveInfo
+
+            // Find removable disks
+            var drives = DriveInfo.GetDrives().Where(x => x.DriveType == DriveType.Removable && x.IsReady);
+
+            List<FileInfo[]> pictureHandles = new List<FileInfo[]>();
+
+            // Find all files of a certain type on each drive
+            foreach (var drive in drives)
+            {
+                pictureHandles.Add(
+                            new DirectoryInfo(drive.Name)
+                            .GetFiles("*.jpg", SearchOption.AllDirectories)
+                            .ToArray());
+            }
+            //var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_USB");
+
+
+            // Do stuff with pictureHandles and identify
+
+
+        }
+        public void GetFirstFiles()
+        {
+            try { 
+            VideoFiles = Directory.GetFiles(Location);
+                foreach(var f in VideoFiles)
+                {
+                    listBox1.Items.Add(Path.GetFileName(f));
+
+
+                }
+            }
+            catch
+            {
+
+                MessageBox.Show("Oculus Quest is not Connected");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+               if(textBox2.Text == "")
+                {
+
+                    MessageBox.Show("Please enter in a file name to pull or check off All");
+                }
+                else
+                {
+                    Oculus_Quest_Video_Downloader.Properties.Settings.Default.ExportsFolder = textBox1.Text;
+                    Oculus_Quest_Video_Downloader.Properties.Settings.Default.Save();
+                    Location = textBox1.Text;
+                    SendCommand();
+
+                }
+               
+            }
+            else
+            {
+                Oculus_Quest_Video_Downloader.Properties.Settings.Default.ExportsFolder = textBox1.Text;
+            Oculus_Quest_Video_Downloader.Properties.Settings.Default.Save();
+            Location = textBox1.Text;
+            SendCommand();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text =  Oculus_Quest_Video_Downloader.Properties.Settings.Default.ExportsFolder ;
+            if (checkBox1.Checked)
+            {
+                button1.Text = "Pull Video Files";
+            }
+            else
+            {
+
+                button1.Text = "Pull Video File";
+
+            }
+            //GetFirstFiles();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) {
+                button1.Text = "Pull Video Files";
+            }
+            else
+            {
+
+                button1.Text = "Pull Video File";
+
+            }
+        }
+    }
+}
